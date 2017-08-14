f64ff1df6cc2aee37ebcf8aaec1debb316c55342
Anubhav Mishra <anubhavmishra@me.com>
2017-08-13 18:44:04 -0700

    lets try again

--- cfgmgmtcamp-demo/main.tf
+++ cfgmgmtcamp-demo/main.tf
@@ -1,12 +1,28 @@
-#### Create EC2 Instances ################################################################
-module "ec2_instance" {
-  source              = "github.com/anubhavmishra/tf_aws_ec2_instance?ref=v0.1.4"
-  instance_type       = "${var.instance_type}"
-  instance_name       = "${var.instance_name_prefix}-${terraform.env}"
-  user_data           = "${path.module}/templates/userdata"
-  subnet_id           = "${element(data.terraform_remote_state.network.public_subnets, 1)}"
-  number_of_instances = "${var.number_of_instances}"
+provider "aws" {
+  region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
