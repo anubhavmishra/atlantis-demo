59b0570a2b16a1fb0cb5b0a5a9a2e2dc866d95eb
Anubhav Mishra <anubhavmishra@me.com>
2017-08-13 18:57:45 -0700

    adding data source

--- cfgmgmtcamp-demo/main.tf
+++ cfgmgmtcamp-demo/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami_ids" "ubuntu" {
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-cd0f5cb6"
   instance_type = "t2.micro"
