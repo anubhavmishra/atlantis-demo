82846395c994ac1438cd67a5f53ffc68724cce1e
Anubhav Mishra <anubhavmishra@me.com>
2017-08-13 18:48:51 -0700

    new ami

--- cfgmgmtcamp-demo/main.tf
+++ cfgmgmtcamp-demo/main.tf
@@ -2,24 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "ami-cd0f5cb6"
   instance_type = "t2.micro"
 
   tags {
