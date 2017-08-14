f5908123bd043ae855f2928043f0ac089a4e5740
Anubhav Mishra <anubhavmishra@me.com>
2017-08-13 18:41:52 -0700

    remove remote state backends

--- cfgmgmtcamp-demo/main.tf
+++ cfgmgmtcamp-demo/main.tf
@@ -1,18 +1,3 @@
-#### Remote state imported from VPC terraform project ###################################
-data "terraform_remote_state" "network" {
-  backend = "s3"
-
-  config {
-    bucket = "cfgmgmtcamp"
-    key    = "vpc/terraform.tfstate"
-    region = "us-west-2"
-  }
-
-  environment = "${terraform.env}"
-}
-
-##########################################################################################
-
 #### Create EC2 Instances ################################################################
 module "ec2_instance" {
   source              = "github.com/anubhavmishra/tf_aws_ec2_instance?ref=v0.1.4"
@@ -23,15 +8,5 @@ module "ec2_instance" {
   number_of_instances = "${var.number_of_instances}"
 }
 
-#### Terraform Remote State Backend ######################################################
-terraform {
-  backend "s3" {
-    bucket = "cfgmgmtcamp"
-    key    = "cfgmgmtcamp-demo/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
-###########################################################################################
 
 
